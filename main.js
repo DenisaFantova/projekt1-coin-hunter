42ced54ecc5ec5e098df17c2b7315cd1179eea83
Denisa Fantová <denisafantova@gmail.com>
2021-03-30 15:34:22 +0200

    Opraveno náhodné umístění mince. Panáček utíká vpravo kousek z obrazovky. Jinak funkčně OK.

--- main.js
+++ main.js
@@ -1,61 +1,71 @@
 
 let panacek = document.getElementById('panacek');
 let mince = document.getElementById('mince');
-let sirkaOkna = window.innerWidth;
-let vyskaOkna = window.innerHeight;
 let zvukMince = document.getElementById('zvukMince');
 let zvukFanfara = document.getElementById('zvukFanfara');
 let score = document.getElementById('score');
+let sirkaOkna = window.innerWidth;
+let vyskaOkna = window.innerHeight;
+console.log(sirkaOkna);
+console.log(vyskaOkna);
 let aktualniScore = 0;
+let panacekSirka = getComputedStyle(panacek);
+panacekSirka = parseInt(panacekSirka.width);
+let panacekVyska = getComputedStyle(panacek);
+panacekVyska = parseInt(panacekVyska.height);
+let minceSirka = getComputedStyle(mince);
+minceSirka = parseInt(minceSirka.width);
+let minceVyska = getComputedStyle(mince);
+minceVyska = parseInt(minceVyska.height);
 
-function hudba() {
-let audio = document.getElementById('hudba');
-audio.play();
-audio.loop = true;
-}
-
-
-function umisteniMince() {
-	nahodneUmisteniX = Math.floor((Math.random() * sirkaOkna) + 0);
-	nahodneUmisteniY = Math.floor((Math.random() * vyskaOkna) + 0);
-	if (nahodneUmisteniX >= sirkaOkna) {
-		return nahodneUmisteniX = nahodneUmisteniX - 36;
+	
+	function hudba() {
+	let audio = document.getElementById('hudba');
+	audio.play();
+	audio.loop = true;
 	}
-	if (nahodneUmisteniY >= vyskaOkna) {
-		return nahodneUmisteniY = nahodneUmisteniY - 36;
-	} 
-	mince.style.left = nahodneUmisteniX + 'px';
-	mince.style.top = nahodneUmisteniY + 'px'; 
-}
 
-umisteniMince();
 
-function umisteniPanacka() {
-	panacek.style.left = sirkaOkna / 2 + 'px';
-	panacek.style.top = vyskaOkna / 2 + 'px';	
-}
+	function umisteniMince() {
+		nahodneUmisteniX = Math.floor((Math.random() * sirkaOkna) + 0);
+		nahodneUmisteniY = Math.floor((Math.random() * vyskaOkna) + 0);
+		if (nahodneUmisteniX >= sirkaOkna) {
+			return nahodneUmisteniX = nahodneUmisteniX - minceSirka;
+		}
+		if (nahodneUmisteniY >= vyskaOkna) {
+			return nahodneUmisteniY = nahodneUmisteniY - minceVyska;
+		} 
+		mince.style.left = nahodneUmisteniX + 'px';
+		mince.style.top = nahodneUmisteniY + 'px'; 
+	}
 
-umisteniPanacka();
+	umisteniMince();
 
-function bod () {
-	aktualniScore = aktualniScore + 1;
-	score.textContent = aktualniScore;
-	if (aktualniScore === 5) {
-		zvukFanfara.play();  
-		alert('Vyhrál jsi. Můžeš si ale hrát dál.');                  
-	} 
-}
+	function umisteniPanacka() {
+		panacek.style.left = sirkaOkna / 2 + 'px';
+		panacek.style.top = vyskaOkna / 2 + 'px';	
+	}
+
+	umisteniPanacka();
+
+
+
+	function bod () {
+		aktualniScore = aktualniScore + 1;
+		score.textContent = aktualniScore;
+		if (aktualniScore === 5) {
+			zvukFanfara.play();  
+			alert('Vyhrál jsi. Můžeš si ale hrát dál.');                  
+		} 
+	}
 
 function pohybPanacka(udalost) {
 	
 	let panacekX = parseInt(panacek.style.left);
 	let panacekY = parseInt(panacek.style.top);
-	let panacekSirka = 64;
-	let panacekVyska = 70;	
 	let minceX = parseInt(mince.style.left);
 	let minceY = parseInt(mince.style.top);
-	let minceSirka = 36;
-	let minceVyska = 36;
+	
 
 	if (udalost.key === 'ArrowUp') {
 		let posunY = panacekY - 10;
@@ -75,7 +85,7 @@ function pohybPanacka(udalost) {
 		let posunY = panacekY + 10;
 		panacek.style.top = posunY + 'px';
 		panacek.src = 'obrazky/panacek.png';
-		if ((panacekY + 64) >= vyskaOkna) {
+		if ((panacekY + panacekVyska) >= vyskaOkna) {
 			panacek.style.top = panacekY + 'px';
 		}
 		if (!(panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || panacekY + panacekVyska < minceY || minceY + minceVyska < panacekY)) {
@@ -103,7 +113,7 @@ function pohybPanacka(udalost) {
 		let posunX = panacekX + 10;
 		panacek.style.left = posunX + 'px';
 		panacek.src = 'obrazky/panacek-vpravo.png';
-		if ((panacekX + 70) >= sirkaOkna) {
+		if ((panacekX + panacekSirka) >= sirkaOkna) {
 			panacek.style.left = panacekX + 'px';
 		}
 		if (!(panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || panacekY + panacekVyska < minceY || minceY + minceVyska < panacekY)) {
@@ -113,6 +123,7 @@ function pohybPanacka(udalost) {
 		}
 	}
 }
+
 
 
 
